ecb4feb1e891f82e099f8579e9830459175d27a6
nitya <nityanandabehera85132@gmail.com>
2022-07-24 00:43:46 +0530

    host

--- wheather/settings.py
+++ wheather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9_(+i^vn*^3jgc6%g9=dew-d25svyk25u8h*v@g72&^2+ddl=t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
